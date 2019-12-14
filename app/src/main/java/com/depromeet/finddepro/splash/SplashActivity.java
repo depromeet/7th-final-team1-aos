e6aa4b63d4e356b99c20f345c74aa54e3541aa89
jonghyo <dlwhdgy9190@gmail.com>
2019-12-14 14:42:08 +0900

    splash activity add

---
+++ app/src/main/java/com/depromeet/finddepro/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.depromeet.finddepro.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.depromeet.finddepro.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+	}
+}
